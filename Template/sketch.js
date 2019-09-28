928cf15ff3a053296a111751209fdd22d508be1b
Ahmed Mohammed Abdelaziz Hasan <wolfrine97@gmail.com>
2019-09-28 22:36:35 +0200

    Update sketch.js

--- Template/sketch.js
+++ Template/sketch.js
@@ -14,12 +14,14 @@ var jumpSound;
 
 //x and y for the player
 var cor={
-    x:50,
-    y:50
-    
-  };
+  x:50,
+  y:50,
+  z:0
+  
+};
 
-  var speed=3;
+
+var speed=0.2;
 
 function preload()
 {
@@ -44,34 +46,52 @@ function keyPressed()
 }
 
 function draw() {
-    background(220);
-    rectMode(CENTER);
-    cor.x=map(cor.x,0,width,0,width,true);
-    cor.y=map(cor.y,0,height,0,height,true);
-    rect(cor.x,cor.y,50,50);
-    move();
+  background(220);
+  rectMode(CENTER);
+  cor.x=map(cor.x,0,width,0,width,true);
+  cor.y=map(cor.y,0,height,0,height,true);
+  
+  
+  push()
+  translate(cor.x,cor.y);
+  rotate(cor.z);
+  rect(0,0,50,50);
+  pop();  
+  
+  rect(300,300,50,50);
+  move(cor);
 }
 
 //the move function it uses the arrows
 function move(){
-    if(keyIsDown(LEFT_ARROW))
-    {
-      cor.x=cor.x-speed;
-    }
-    
-    if(keyIsDown(RIGHT_ARROW))
-    {
-      cor.x=cor.x+speed;
-    }
-    
-    if(keyIsDown(UP_ARROW))
-    {
-      cor.y=cor.y-speed;
-    }
-    
-    if(keyIsDown(DOWN_ARROW))
-    {
-      cor.y=cor.y+speed; 
-    }
+  if(keyIsDown(LEFT_ARROW))
+  {
+    cor.x=cor.x-speed*deltaTime;
+  }
+  
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    cor.x=cor.x+speed*deltaTime;
+  }
+  
+  if(keyIsDown(UP_ARROW))
+  {
+    cor.y=cor.y-speed*deltaTime;
+  }
+  
+  if(keyIsDown(DOWN_ARROW))
+  {
+    cor.y=cor.y+speed*deltaTime;
+  }
+  
+  if(keyIsDown(82))
+     {
+      cor.z=cor.z+1;
+     }
+  
+  if(keyIsDown(81))
+  {
+    cor.z=cor.z-1;
+  }
     
   }
