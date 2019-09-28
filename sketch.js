d39f4323337ee9a5ee1b8529766803e393e74965
Ahmed Mohammed Abdelaziz Hasan <wolfrine97@gmail.com>
2019-09-28 21:08:02 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,29 @@ function setup()
 function draw()
 {
     background(0);
-    scale(2);
-    ellipse(width/2, height/2, 50);
+    ellipse(width/2, height/2, 50)
 }
+
+
+function move(){
+    if(keyIsDown(LEFT_ARROW))
+    {
+      cor.x=cor.x-speed;
+    }
+    
+    if(keyIsDown(RIGHT_ARROW))
+    {
+      cor.x=cor.x+speed;
+    }
+    
+    if(keyIsDown(UP_ARROW))
+    {
+      cor.y=cor.y-speed;
+    }
+    
+    if(keyIsDown(DOWN_ARROW))
+    {
+      cor.y=cor.y+speed;
+    }
+    
+  }
