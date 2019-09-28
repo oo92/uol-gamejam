0c466beb6b676e7c12209e95061717723c644c5f
m-mood <matthew.moodliar@gmail.com>
2019-09-28 23:56:44 +0200

    Add player sprite and projectiles

--- sketch.js
+++ sketch.js
@@ -1,21 +1,66 @@
+var projectiles = [];
+
 var player =  {x: 500, y: 500, rotation: 0, dy: 0, dx: 0};
+var playerSprite
+
+function preload()
+{
+    playerSprite = loadImage('assets/ship.png');
+}
 
 function setup()
 {
-	createCanvas(windowWidth - 50, windowHeight - 50);
+    createCanvas(windowWidth - 50, windowHeight - 50);
+}
+ 
+function keyPressed()
+{
+    
+    if(keyCode == 32)
+    {
+        projectiles.push(new Projectile(player.x + player.dx, player.y + player.dy, player.rotation));
+    }
 }
 
 function draw()
 {
     clear();
-    background(0);
+    background(120,200,200);
     
+    for(var i = 0; i < projectiles.length; i++)
+    {
+        projectiles[i].drawProjectile();
+        
+        if(projectiles[i].shipX < 0 || projectiles[i].shipX > width || projectiles[i].shipY < 0 || projectiles[i].shipY > height)
+        {
+            projectiles.splice(i,1);
+            i--;
+        }
+    }
     
-    // Draw player ship
+    push();
     translate(player.x + player.dx, player.y + player.dy)
     rotate(player.rotation);
-    ellipse(0, 0, 50, 180);
+    noSmooth();
+    imageMode(CENTER);
+    image(playerSprite, 0, 0, 17 * 2, 64 * 2)
+    //ellipse(0, 0, 50, 180);
     shipSteerControls();
+    pop();
+}
+
+function Projectile(shipX, shipY, shipAng)
+{
+    this.shipX = shipX;
+    this.shipY = shipY;
+    this.projAng = shipAng + PI;
+    this.drawProjectile = function()
+    {
+        fill(255);
+        rect(this.shipX, this.shipY, 10, 10);
+        this.shipX +=  cos(this.projAng);
+        this.shipY += sin(this.projAng);
+    }
 }
 
 function shipSteerControls()
