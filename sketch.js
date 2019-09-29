ec351443c53e84a123a70b0888272e86d9f1adb0
m-mood <matthew.moodliar@gmail.com>
2019-09-29 05:45:29 +0200

    Add enemy spawns

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 var projectiles = [];
 var enemyProjectiles = [];
 
-// Player object and sprite
-var player =  {x: 500, y: 500, rotation: 0, dy: 0, dx: 0, projRange: 200, maxRange: 400, minRange: 30, hitBoxWidth: 50, hitBoxHeight: 180};
-var playerSprite
+var enemies = [];
 
-var testEnemy = {x: 200, y:200, radius: 100};
+
+// Player object and sprite
+var player =  {x: 500, y: 500, rotation: 0, dy: 0, dx: 0, projRange: 200, maxRange: 400, minRange: 30, hitBoxRadius: 50, lives: 3, ammo: 5};
+var playerSprite;
+
+var reloadTime = 0;
+var reloading = false;
+var projTimeCounter = 0;
+var enemySpawnTimer = 0;
+var numOfEnemy = 0;
+var jumpSound;
 
 // Player's real position in the world
 var player_world = {x: 0, y: 0};
@@ -13,15 +21,21 @@ var player_world = {x: 0, y: 0};
 var scrollPos = {x: 0, y: 0};
 
 //Powerups
-var moveSpeed=1;
-var rotationSpeed=0.02;
-var projectileSpeed=1;
-var powerBool=true;
+var moveSpeed = 1;
+var rotationSpeed = 0.02;
+var projectileSpeed = 1;
+var powerBool = true;
 var speedUpSprite;
 var shotUpSprite;
 
-function preload()
+function preload() 
 {
+    //soundFormats('mp3', 'wav');
+    
+    //load your sounds here
+    //jumpSound = loadSound('assets/jump.wav');
+    //jumpSound.setVolume(0.1);
+
     // Load player sprite from file
     playerSprite = loadImage('assets/ship.png');
     shotUpSprite = loadImage('assets/shotsup.png');
@@ -40,12 +54,23 @@ function keyPressed()
     //If space is pressed instatiate a new projectile
     if(keyCode == 32)
     {
-        projectiles.push(new Projectile(player.x + player.dx, player.y + player.dy, player.rotation));
+        if(player.ammo > 0 && projTimeCounter > 50 && !reloading)
+        {
+            projectiles.push(new Projectile(player.x + player.dx, player.y + player.dy, player.rotation));  
+            player.ammo--;
+            projTimeCounter = 0;
+        }
     }
     
     if(keyCode == 71)
     {
         enemyProjectiles.push(new Projectile(100, 400, PI));
+    }
+    
+    if(keyCode == 82 && reloading == false)
+    {
+        reloading = true;
+        reloadTime = 0;
     }
 }
 
@@ -54,12 +79,35 @@ function draw()
     clear();
     background(120,200,200);
 
+    reloadTime++;
+    projTimeCounter++;
+    enemySpawnTimer++;
     
     
     // Push scrolling background
     push();
     // Scroll background
     translate(scrollPos.x, scrollPos.y);
+    
+    if(reloading == true)
+    {
+        push();
+        fill(255,100,100);
+        text("Reloading!", 300, 50);
+        if(reloadTime > 300)
+        {
+            player.ammo = 5;
+            reloading = false;
+        }
+        pop();
+    }
+    
+    if(enemySpawnTimer > 100 && numOfEnemy < 3)
+    {
+        enemies.push(new Enemy(random(0, width), random(0, height)));
+        enemySpawnTimer = 0;
+        numOfEnemy++;
+    }
     
     for(var i = 0; i < projectiles.length; i++)
     {
@@ -72,13 +120,26 @@ function draw()
         {
             projectiles.splice(i,1);
             i--;
+        }else{
+            for(var j = 0; j < enemies.length; j++)
+            {
+                if(dist(enemies[j].x, enemies[j].y, projectiles[i].shipX, projectiles[i].shipY) < enemies[j].radius/2 && dist(enemies[j].x, enemies[j].y, projectiles[i].projEndPointX, projectiles[i].projEndPointY) < enemies[j].radius/2)
+                {
+                    projectiles.splice(i,1);
+                    i--;  
+                    enemies.splice(j,1);
+                    j--;
+                    break;
+                }
+            }
         }
         //If the projectile is within the distance threshold of the enemy and the endpoint of the projectile is within the threshold distance of the enemy remove the projectile
-        else if(dist(testEnemy.x, testEnemy.y, projectiles[i].shipX, projectiles[i].shipY) < 50 && dist(testEnemy.x, testEnemy.y, projectiles[i].projEndPointX, projectiles[i].projEndPointY) < 50)
-        {
-            projectiles.splice(i,1);
-            i--;  
-        }
+    }
+    
+    for(var i = 0; i < enemies.length; i++)
+    {
+        enemies[i].drawEnemy();
+        enemies[i].drawRadius();
     }
 
     for(var i = 0; i < enemyProjectiles.length; i++)
@@ -92,12 +153,14 @@ function draw()
             enemyProjectiles.splice(i,1);
             i--;
         }
-       // else if(abs(enemyProjectiles[i].shipX - player.x - player.dx) < player.hitBoxWidth/2 * sin(player.rotation)&& abs(enemyProjectiles[i].shipY - player.y - player.dy) < player.hitBoxHeight/2 )
-        else if(abs(player.x + player.dx - (sin(player.rotation) * player.hitBoxHeight) - enemyProjectiles[i].shipX) < player.hitBoxWidth/2 * cos(player.rotation) + player.hitBoxHeight * sin(player.rotation)
-               )
+        else if(dist(player.x + player.dx, player.y + player.dy, enemyProjectiles[i].shipX, enemyProjectiles[i].shipY) < player.hitBoxRadius/2)
         {
             enemyProjectiles.splice(i,1);
-            i--;  
+            i--; 
+            if(player.lives > 0)
+            {
+                player.lives--;
+            }
         }
         
         pop();
@@ -114,21 +177,12 @@ function draw()
     //Draw player hitbox - delete later
     noFill();
     stroke(255,0,0);
-    rect(-player.hitBoxWidth/2,-player.hitBoxHeight/2,player.hitBoxWidth,player.hitBoxHeight);
+    ellipse(0,0, player.hitBoxRadius);
     //
     pop();
     
     //Draws the line representing the path of the projectile
     drawProjectilePath();
-
-    //draws a test enemy
-    push();
-    fill(255);
-    ellipse(testEnemy.x, testEnemy.y, 5);
-    noFill();
-    stroke(255);
-    ellipse(testEnemy.x, testEnemy.y, testEnemy.radius);
-    pop();
     
     //powerups it provides two options and you can choose one and the other disappears
     powerup=new powerUps(500,600,600,500);
@@ -148,6 +202,7 @@ function draw()
         projectileSpeed=3;
         powerBool=false;
     }
+
     
     // Player input
     shipSteerControls();
@@ -169,6 +224,11 @@ function draw()
 
     // Pop background scrolling
     pop();
+    
+    textSize(30);
+    fill(255);
+    text("Lives: " + player.lives, 50, 50);
+    text("Ammo: " + player.ammo, 50, 90);
 }
 
 //Constructor function for creating new projectiles
@@ -202,8 +262,9 @@ function Projectile(shipX, shipY, shipAng)
         //increments x and y by the angle of the projectile so it continues on correct path
         this.shipX +=  cos(this.projAng) * this.projSpeed;
         this.shipY += sin(this.projAng) * this.projSpeed;
-    }
+    }   
 }
+
 
 // Player input
 // Left and Right rotate ship
@@ -259,7 +320,6 @@ function powerUps(moveX,moveY,projX,projY)
     this.moveY=moveY;
     this.projX=projX;
     this.projY=projY;
-
     this.moveUp =function()
     {
         push();
@@ -268,13 +328,34 @@ function powerUps(moveX,moveY,projX,projY)
         image(speedUpSprite, this.moveX, this.moveY, 13 * 2, 16 * 2)
         pop();
     }
-
     this.projectileUp=function()
     {
         push();
         noSmooth();
         imageMode(CENTER);
         image(shotUpSprite, this.projX, this.projY, 13 * 2, 13 * 2)
+        pop();
+    }
+}
+
+function Enemy(x,y)
+{
+    this.x = x;
+    this.y = y;
+    this.radius = 50;
+    this.drawEnemy = function()
+    {
+        push();
+        fill(255,0,0);
+        ellipse(this.x, this.y, this.radius, 180);
+        pop();
+    },
+    this.drawRadius = function()
+    {
+        push();
+        fill(0,255,0);
+        noFill();
+        ellipse(this.x, this.y, this.radius );
         pop();
     }
 }
